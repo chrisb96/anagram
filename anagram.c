6fbf7222b4e712e73e2506a024a32121fb0d8c4f
Zoe Becker <chrisb96@gmail.com>
2019-05-15 21:47:35 -0400

    Update anagram.c
    added comments

--- anagram.c
+++ anagram.c
@@ -7,20 +7,22 @@
 
 #include "stdio.h"
 
+
 int main(void){
-    
+    //set up variables
     int arr[26] = {0};
     int count = 0;
     char ch;
     
-    
+    //prompy user for input
     printf("Enter a word : ");
     
+    //adds the numeric character value to the array of ints until they press enter
     while( (ch = getchar() ) != '\n' ){
         arr[ch - 'a']++;
     }
     
-    
+    //subtracts the numeric character value from the array.
     printf("Enter a another word : ");
     
     while( (ch = getchar() ) != '\n' ){
@@ -29,11 +31,13 @@ int main(void){
     
     
     for(int i = 0; i < 26; i++){
+        //an array value of 0 means the same character was added and subtracted
         if(arr[i] == 0){
             count++;
         }
+        
     }
-    
+    //results
     if(count == 26){
         printf("the words you entered are anagrams \n");
     }
